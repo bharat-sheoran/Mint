eab8957fd206a71be7ddd415f8e037ecb5f40f1e
Bharat Sheoran <bharatsheoran03@gmail.com>
2024-01-03 11:24:48 +0530

    UI Enhanced

--- Frontend/src/components/ManageList.jsx
+++ Frontend/src/components/ManageList.jsx
@@ -4,18 +4,28 @@ export default function ManageList({ manage }) {
 
     return (
         <>
-            {manage.map((d) => (
-                <li key={d._id}>
-                    <div className="manageList">
-                        name: {d.name}<br />
-                        category: {d.category}<br />
-                        date: {d.date}<br />
-                        used: {d.used}<br />
-                        availaible: {d.availaible}<br />
-                        <br />
-                    </div>
-                </li>
-            ))}
+            <table className="manage-list">
+                <thead className='manage-thead'>
+                    <tr>
+                        <th>Date</th>
+                        <th>Category</th>
+                        <th>Name</th>
+                        <th>Used</th>
+                        <th>Availaible</th>
+                    </tr>
+                </thead>
+                <tbody className='manage-tbody'>
+                    {manage.map((d) => (
+                        <tr key={d._id}>
+                            <td>{d.date.toString().slice(0, 10)}</td>
+                            <td>{d.category}</td>
+                            <td>{d.name}</td>
+                            <td>{d.used}</td>
+                            <td>{d.availaible}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </>
     )
 }
--- Frontend/src/pages/Manage.jsx
+++ Frontend/src/pages/Manage.jsx
@@ -1,21 +1,22 @@
 import axios from "axios";
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import ManageList from "../components/ManageList";
 
 export default function Manage() {
     let [manage, setManage] = useState([]);
 
-    const getData = async () => {
-        let data = await axios.get("http://localhost:8080/manage");
-        setManage(data.data);
-    }
+    useEffect( () => {
+        async function getData(){
+            let data = await axios.get("http://localhost:8080/manage");
+            setManage(data.data);
+        }
+        getData();
+    }, []);
+
 
     return (
         <>
-            <button onClick={getData}>Button</button>
-            <ul>
-                <ManageList manage={manage} />
-            </ul>
+            <ManageList manage={manage} />
         </>
     )
 }
